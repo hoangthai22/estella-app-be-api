fc72343accd0e3fa0ab9e82f999cfada8a3f6b78
hoangthai22 <hoangthai22tv@gmail.com>
2022-01-02 15:45:39 +0700

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3000;
 const bootServer = () => {
   const app = express();
   const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000" || "http://192.168.1.8:3000",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   };
   app.use(cors(corsOptions));
